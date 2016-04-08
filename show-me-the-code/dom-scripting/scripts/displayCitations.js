6b44a111769d0a1430e96a42b5472828e2bc258b
Liu Zhuan <liuzhuankyzy@gmail.com>
2016-04-08 10:17:57 +0800

    add more dom scripting

---
+++ show-me-the-code/dom-scripting/scripts/displayCitations.js
@@ -0,0 +1,22 @@
+function displayCitations() {
+	var quotes = document.getElementsByTagName("blockquote")
+	for (var i = 0; i < quotes.length; i++) {
+		if (!quotes[i].getAttribute("cite")) continue;
+		var url = quotes[i].getAttribute("cite")
+
+		var quoteElements = quotes[i].getElementsByTagName("*")
+		if (quoteElements.length < 1) continue
+		var elem = quoteElements[quoteElements.length - 1];
+
+		var link = document.createElement("a")
+		var link_text = document.createTextNode("source")
+		link.appendChild(link_text)
+		link.setAttribute("href", url)
+
+		var superscript = document.createElement("sup")
+		superscript.appendChild(link)
+		elem.appendChild(superscript)
+	}
+}
+
+addLoadEvent(displayCitations)
