abc1c5ddf37ad1a56f18b5cf8329c95593357e85
liuz <liuzhuankyzy@gmail.com>
2016-11-27 21:24:25 +0800

    add some typescript demo.

---
+++ snippet/typescript-in-action/hello/index.js
@@ -0,0 +1,11 @@
+var Startup = (function () {
+    function Startup() {
+    }
+    Startup.main = function (name) {
+        console.log("Hello " + name + "!");
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main('Monica');
+//# sourceMappingURL=index.js.map
