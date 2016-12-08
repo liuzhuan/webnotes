042fbc5200de62e6ee22f63b5802f78c97e87b93
liuz <liuzhuankyzy@gmail.com>
2016-12-09 07:41:24 +0800

    learn typescript

--- snippet/typescript-in-action/hello/index.js
+++ snippet/typescript-in-action/hello/index.js
@@ -1,11 +1,1 @@
-var Startup = (function () {
-    function Startup() {
-    }
-    Startup.main = function (name) {
-        console.log("Hello " + name + "!");
-        return 0;
-    };
-    return Startup;
-}());
-Startup.main('Monica');
-//# sourceMappingURL=index.js.map
+console.log(__dirname)
