9ef88c46a964ae21194dec33b254896b09aee33d
liuz <liuzhuankyzy@gmail.com>
2017-01-11 06:31:48 +0800

    set up a tiny chat server

--- snippet/nodejs/chat-server/app.js
+++ snippet/nodejs/chat-server/app.js
@@ -1,7 +1,12 @@
-var http = require('http');
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(8124, '127.0.0.1');
+var net = require('net');
 
-console.log('Server running at http://127.0.0.1:8124');
+var chatServer = net.createServer();
+
+chatServer.on('connection', function(client){
+    client.write('Hi!\n');
+    client.write('Bye!\n');
+
+    client.end();
+});
+
+chatServer.listen(9000);
